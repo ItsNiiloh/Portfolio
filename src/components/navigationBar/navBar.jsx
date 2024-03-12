550ea65093047b5f8ea514597f9ea1acdae1514c
Jose <zeferreira1458@hotmail.com>
2024-03-12 17:40:34 +0100

    Add animation to nav

--- src/components/navigationBar/navBar.jsx
+++ src/components/navigationBar/navBar.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
+import { motion, AnimatePresence, useScroll, useMotionValueEvent,} from "framer-motion";
 import NavItems from "./navItems";
 
 const variants = {
   open: {
-    width: 400,
-    height: 600,
+    width: 350,
+    height: 550,
     top: 25,
     left: 15,
     transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1] },
@@ -20,8 +20,23 @@ const variants = {
 };
 
 const NavBar = React.forwardRef(({ isActive, setIsActive }, ref) => {
+  const { scrollY } = useScroll();
+  const [hidden, setHidden] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latestScrollY) => {
+    const prevScrollY = scrollY.getPrevious();
+    if (latestScrollY > prevScrollY && latestScrollY > 100) setHidden(true);
+    else setHidden(false);
+  });
+
   return (
-    <header ref={ref} className="absolute z-50">
+    <motion.nav
+      variants={{ hidden: { opacity: 0 }, visible: { opacity: 100 } }}
+      animate={hidden ? "hidden" : "visible"}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+      ref={ref}
+      className="sticky z-50"
+    >
       <motion.div
         className="fixed overflow-hidden rounded-3xl bg-gradient-to-br from-[#1f1f1f] to-[#0a0a0a] border border-[#2d2d2d] opacity-95 shadow-lg"
         variants={variants}
@@ -42,26 +57,27 @@ const NavBar = React.forwardRef(({ isActive, setIsActive }, ref) => {
           animate={{ top: isActive ? "-100%" : "0" }}
           transition={{ duration: 0.5, ease: [0.76, 0, 0.24, 1] }}
         >
-          
-    <div className="h-full w-full flex justify-center items-center bg-white uppercase text-black text-lg font-semibold tracking-wide group">
-      <NavbarText label={"Menu"}/>
-    </div>
-                    
-    <div className="h-full w-full flex justify-center items-center bg-black uppercase text-white text-lg font-semibold tracking-wide group">
-      <NavbarText label={"Close"}/>
-    </div>
+          <div className="h-full w-full flex justify-center items-center bg-white uppercase text-black text-lg font-semibold tracking-wide group">
+            <NavbarText label={"Menu"} />
+          </div>
+
+          <div className="h-full w-full flex justify-center items-center bg-black uppercase text-white text-lg font-semibold tracking-wide group">
+            <NavbarText label={"Close"} />
+          </div>
         </motion.div>
       </nav>
-    </header>
+    </motion.nav>
   );
 });
 
-function NavbarText ({label}) {
+function NavbarText({ label }) {
   return (
     <>
-      <p className="absolute scale-100 group-hover:scale-125 duration-300">{label}</p>
+      <p className="absolute scale-100 group-hover:scale-125 duration-300">
+        {label}
+      </p>
     </>
-  )
+  );
 }
 
 export default NavBar;
